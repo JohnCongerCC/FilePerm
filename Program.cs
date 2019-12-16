e5177ff418859fc7b447b3a75469dda83f939ff6
johncongercc <johnconger@gmail.com>
2019-12-16 15:42:02 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace FilePerm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderPath = @"c:\Search";
+            foreach (string f in Directory.GetFiles(folderPath))
+            {
+               FilePerm(f);
+            }
+            
+            //DirSearch(folderPath);
+            
+        }
+
+        static void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        FilePerm(f);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        static void FilePerm(string FilePath)
+        {
+            var security = new FileSecurity(FilePath, AccessControlSections.Owner | AccessControlSections.Group | AccessControlSections.Access);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Crypto
+The purpose of this app is to demo the implementation of https://cryptopals.com/
+
+## To begin using this repo 
+#### Follow these steps to set up this application
+Note: This application requires dotnet core version 2.2 running on a Mac on Linux (should run on Windows too minus the shell script)
+``` bash
+  git clone https://github.com/JohnCongerCC/crypto.git
+  cd crypto
+  ./test.sh
+```
